1a9057ff578ac3cdde6e57558f22ade33ead0f53
Simone <simonee240294@gmail.com>
2021-05-14 16:40:33 +1000

    Contact Form Base Complete

--- contact_form/ContactFormLayout.php
+++ contact_form/ContactFormLayout.php
@@ -4,7 +4,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="sass/stylesheet.css">
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title><?=$title?></title><!-- h1, center -->
     </head>
     <body>
@@ -14,7 +15,7 @@
                     <img src="" alt="">
                 </h1>
             </header>
-            <div><!-- div contact box-->
+            <div class="contact-box">
             <?= $output ?>
             </div>
             <footer>
